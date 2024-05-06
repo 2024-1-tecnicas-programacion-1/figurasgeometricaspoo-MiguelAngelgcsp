ac589dce1ff3bf6d1d581e6a7a741e0c5c16e07d
juansegarod <juansega96@gmail.com>
2024-05-06 10:09:10 -0500

    Se agrega codigo de Taller 4 con cambio en el archivo pom.xml

--- src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
@@ -7,7 +7,8 @@ public class Rectangulo
     private double lado1;
     private double lado2;
     
-    public Rectangulo( double lado1, double lado2) {
+    public Rectangulo( double lado1, double lado2)
+    {
         
     this.lado1 = lado1;
     this.lado2 = lado2;
